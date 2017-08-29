e5a9701cbde3dca4361b267336213fdfa4b392c5
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-08-28 21:00:17 -0700

    Primeiro commit

---
+++ src/uem/mz/teste/Teste.java
@@ -0,0 +1,16 @@
+
+package uem.mz.teste;
+
+/**
+ *
+ * @author Alfredo
+ */
+public class Teste {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Ola git e gitHub");
+    }
+    
+    
+}
